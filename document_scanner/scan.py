2e6cee6b5395ec15d63fd65da1096a9302eda70a
snorlax_codes <siarpit119@gmail.com>
2025-04-08 22:48:50 +0530

    adding dependencies

--- document_scanner/scan.py
+++ document_scanner/scan.py
@@ -2,11 +2,11 @@ from pyimagesearch.transform import four_point_transform
 from skimage.filters import threshold_local
 import numpy as np
 import argparse
-import cv2
+import cv2 as cv
 import imutils
 
+# construct the argument parser and prase the arguments
 ap = argparse.ArgumentParser()
-ap = add_argument("-i", "--image", required = True,
-                  help = "path to the image to be scanned")
+ap.add_argument("-i", "--image", required = True,
+                help = "path to the image to be scanned")
 args = vars(ap.parse_args())
-ap.add_argumenmt
